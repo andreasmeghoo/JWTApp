1d1d063cad49f116273656c6306a8bdd2de78fdf
Andreas Meghoo <andyjmeghoo@gmail.com>
2025-07-13 16:43:13 -0500

    Create Endpoints for Admins to View Registered Users, who are approved and unapproved.

---
+++ JWTApp/JWTApp/Controllers/AdminController.cs
@@ -0,0 +1,52 @@
+﻿using JWTApp.Data;
+using JWTApp.Models;
+using JWTApp.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTApp.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "Admin")]
+    public class AdminController
+    {
+
+        private readonly JWTAppDBContext _context;
+
+        public AdminController(JWTAppDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("UnapprovedUsers")]
+        public ActionResult<List<UserViewModel>>GetUnapprovedUsers()
+        {
+            return _context.Users.Where(x => !x.IsApproved)
+                .Select(x => new UserViewModel
+                {
+                    Id = x.Id,
+                    Username = x.Username,
+                    Role = x.Role,
+                    IsApproved = x.IsApproved
+                })
+                .ToList(); 
+        }
+
+        [HttpGet("ApprovedUsers")]
+        public ActionResult <List<UserViewModel>>GetApprovedUsers()
+        {
+            return _context.Users.Where(x => x.IsApproved)
+              .Select(x => new UserViewModel
+              {
+                  Id = x.Id,
+                  Username = x.Username,
+                  Role = x.Role,
+                  IsApproved = x.IsApproved
+              })
+              .ToList();
+        }
+
+    }
+}
---
+++ JWTApp/JWTApp/ViewModels/UserViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace JWTApp.ViewModels
+{
+    public class UserViewModel
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Role { get; set; } = "User";
+
+        public bool IsApproved { get; set; } = false;
+    }
+}
